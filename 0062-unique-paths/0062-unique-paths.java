fb6091ee36c094975a9b0bbd26ea0779fbff4c31
5055_HARIPRASATH N <hariprasath02078@gmail.com>
2026-08-16 11:37:13 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.25%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+
+        int[] a = new int[n];
+        Arrays.fill(a, 1);
+
+        for (int i = 1; i < m; i++) {
+
+            int[] b = new int[n];
+            Arrays.fill(b, 1);
+
+            for (int j = 1; j < n; j++) {
+                b[j] = b[j - 1] + a[j];
+            }
+
+            a = b;
+        }
+
+        return a[n - 1];
+    }
+}
